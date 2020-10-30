76e2eb67c18159807fc5ce9d4432a4ee7de6c43a
siawashkasra <siawashkasra@gmail.com>
2020-10-30 15:45:19 +0430

    Add Todo Form

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from .models import Todo
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -10,3 +11,6 @@ todos = Todo.objects.all()
 def index(request):
     return render(request, 'todos/index.html', {'todos': todos})
     
+def create(request):
+    print(request)
+    return HttpResponse('This is create!')
